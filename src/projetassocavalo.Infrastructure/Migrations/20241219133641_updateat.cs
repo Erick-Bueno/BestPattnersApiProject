e642821a95df2c9eafc98dd13181ccd03dac1712
Erick bueno <erickjb93@gmail.com>
2024-12-19 10:38:10 -0300

    feat: add updateat migration

---
+++ src/projetassocavalo.Infrastructure/Migrations/20241219133641_updateat.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace projetassocavalo.Infrastructure.Migrations
+{
+    public partial class updateat : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

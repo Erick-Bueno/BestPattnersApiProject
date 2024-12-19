79a93c1032756045b1b989ed93803c9d7daacaed
Erick bueno <erickjb93@gmail.com>
2024-12-19 10:35:48 -0300

    refactor: remove set datetime now of updated at

--- src/projetassocavalo.Domain/Entities/Entity.cs
+++ src/projetassocavalo.Domain/Entities/Entity.cs
@@ -4,5 +4,5 @@ public class Entity
 {
     public Guid Id { get; set; } = Guid.NewGuid();
     public DateTime CreatedAt { get; private set; } =  DateTime.UtcNow;
-    public DateTime UpdatedAt { get; private set; } =  DateTime.UtcNow;
+    public DateTime UpdatedAt { get; private set; }
 }
